81bfe73fb753fafa3f4d56586dcede30e1e39b4c
Ahamed <ahamed.irfan001@gmail.com>
2024-05-18 17:23:24 +0530

    Order

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 // import logo from './logo.svg';
 import "./App.css";
 // import Cart from "./Client/Components/Cart/Cart";
-import Checkout from "./Client/Components/Checkout/Checkout";
+// import Checkout from "./Client/Components/Checkout/Checkout";
 import Footer from "./Client/Components/Footer/Footer";
 import Navigation from "./Client/Components/Navigation/Navigation";
+import OrderDetails from "./Client/Components/Order/OrderDetails";
+// import Order from "./Client/Components/Order/Order";
 // import Product from "./Client/Components/Product/Product";
 // import ProductDetails from "./Client/Components/ProductDetails/ProductDetails";
 // import HomePage from "./Client/Pages/Home Page/HomePage";
@@ -17,7 +19,9 @@ function App() {
         {/* <Product/> */}
         {/* <ProductDetails/> */}
         {/* <Cart /> */}
-        <Checkout/>
+        {/* <Checkout/> */}
+        {/* <Order/> */}
+        <OrderDetails/>
       </div>
       <Footer />
     </div>
