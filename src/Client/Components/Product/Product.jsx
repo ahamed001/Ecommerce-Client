76d84f6283124b11d0aae626a32d60a7fad24909
Ahamed001 <ahamed.irfan001@gmail.com>
2024-06-19 18:51:08 +0530

    redux

--- src/Client/Components/Product/Product.jsx
+++ src/Client/Components/Product/Product.jsx
@@ -1,6 +1,4 @@
-import { Fragment, useState } from "react";
 import { Dialog, Disclosure, Menu, Transition } from "@headlessui/react";
-import { XMarkIcon } from "@heroicons/react/24/outline";
 import {
   ChevronDownIcon,
   FunnelIcon,
@@ -8,8 +6,8 @@ import {
   PlusIcon,
   Squares2X2Icon,
 } from "@heroicons/react/20/solid";
-import { mens_kurta } from "../../../Data/Mens_Kurta";
-import ProductCard from "./ProductCard";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import FilterListIcon from "@mui/icons-material/FilterList";
 import {
   FormControl,
   FormControlLabel,
@@ -17,8 +15,10 @@ import {
   Radio,
   RadioGroup,
 } from "@mui/material";
-import FilterListIcon from "@mui/icons-material/FilterList";
+import { Fragment, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { mens_kurta } from "../../../Data/Mens_Kurta";
+import ProductCard from "./ProductCard";
 
 const sortOptions = [
   { name: "Price: Low to High", href: "#", current: false },
@@ -320,6 +320,66 @@ export default function Product() {
                   <FilterListIcon />
                 </div>
                 <form className="hidden lg:block">
+                  {filters.map((section) => (
+                    <Disclosure
+                      as="div"
+                      key={section.id}
+                      className="border-b border-gray-200 py-6"
+                    >
+                      {({ open }) => (
+                        <>
+                          <h3 className="-my-3 flow-root">
+                            <Disclosure.Button className="flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
+                              <span className="font-medium text-gray-900">
+                                {section.name}
+                              </span>
+                              <span className="ml-6 flex items-center">
+                                {open ? (
+                                  <MinusIcon
+                                    className="h-5 w-5"
+                                    aria-hidden="true"
+                                  />
+                                ) : (
+                                  <PlusIcon
+                                    className="h-5 w-5"
+                                    aria-hidden="true"
+                                  />
+                                )}
+                              </span>
+                            </Disclosure.Button>
+                          </h3>
+                          <Disclosure.Panel className="pt-6">
+                            <div className="space-y-4">
+                              {section.options.map((option, optionIdx) => (
+                                <div
+                                  key={option.value}
+                                  className="flex items-center"
+                                >
+                                  <input
+                                    onChange={() =>
+                                      handleFilter(option.value, section.id)
+                                    }
+                                    id={`filter-${section.id}-${optionIdx}`}
+                                    name={`${section.id}[]`}
+                                    defaultValue={option.value}
+                                    type="checkbox"
+                                    defaultChecked={option.checked}
+                                    className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                                  />
+                                  <label
+                                    htmlFor={`filter-${section.id}-${optionIdx}`}
+                                    className="ml-3 text-sm text-gray-600"
+                                  >
+                                    {option.label}
+                                  </label>
+                                </div>
+                              ))}
+                            </div>
+                          </Disclosure.Panel>
+                        </>
+                      )}
+                    </Disclosure>
+                  ))}
                   {SingleFilters.map((section) => (
                     <Disclosure
                       as="div"
