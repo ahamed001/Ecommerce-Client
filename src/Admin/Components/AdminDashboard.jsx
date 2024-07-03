f804e901bc9e7e062911e2a9e3e8703792e26ace
Ahamed001 <ahamed.irfan001@gmail.com>
2024-07-03 18:25:46 +0530

    Achievements

--- src/Admin/Components/AdminDashboard.jsx
+++ src/Admin/Components/AdminDashboard.jsx
@@ -4,7 +4,7 @@ import Achievement from './Achievement'
 
 const AdminDashboard = () => {
   return (
-    <div>
+    <div className='p-10'>
       <Grid container spacing={2}>
         <Grid item xs={12} md={4}>
           <Achievement/>
