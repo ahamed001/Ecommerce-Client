42f21b3c54b6cf0eaab3efc751fa0bb02d6db96c
Ahamed001 <ahamed.irfan001@gmail.com>
2024-07-04 18:37:41 +0530

    Monthly Overview

--- src/Admin/Components/AdminDashboard.jsx
+++ src/Admin/Components/AdminDashboard.jsx
@@ -1,6 +1,7 @@
 import { Grid } from '@mui/material'
 import React from 'react'
 import Achievement from './Achievement'
+import MonthlyOverview from './MonthlyOverview'
 
 const AdminDashboard = () => {
   return (
@@ -8,6 +9,11 @@ const AdminDashboard = () => {
       <Grid container spacing={2}>
         <Grid item xs={12} md={4}>
           <Achievement/>
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <MonthlyOverview/>
+        </Grid>
+        <Grid item xs={12} md={6}>
         </Grid>
       </Grid>
     </div>
