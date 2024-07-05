b479f8008cae882236f02a8fcdec4c77745583e7
Ahamed001 <ahamed.irfan001@gmail.com>
2024-07-05 17:01:50 +0530

    Product Table

--- src/Admin/Components/AdminDashboard.jsx
+++ src/Admin/Components/AdminDashboard.jsx
@@ -2,6 +2,7 @@ import { Grid } from '@mui/material'
 import React from 'react'
 import Achievement from './Achievement'
 import MonthlyOverview from './MonthlyOverview'
+import ProductsTable from './ProductsTable'
 
 const AdminDashboard = () => {
   return (
@@ -14,6 +15,7 @@ const AdminDashboard = () => {
           <MonthlyOverview/>
         </Grid>
         <Grid item xs={12} md={6}>
+          <ProductsTable/>
         </Grid>
       </Grid>
     </div>
